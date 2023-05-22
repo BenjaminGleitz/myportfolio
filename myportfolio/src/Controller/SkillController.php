d82b079f5013928d42b68173edd9ddc5782dfaf0
benjamingleitz <gleitz.benjamin@gmail.com>
2023-05-22 19:11:58 +0200

    création de la skill-page

--- myportfolio/src/Controller/SkillController.php
+++ myportfolio/src/Controller/SkillController.php
@@ -6,16 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'home_')]
-class HomeController extends AbstractController
+#[Route('/skill', name: 'skill_')]
+class SkillController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        $page = 'home';
-        // dd($page);
 
-        return $this->render('home/index.html.twig', [
+        $page = 'skill';
+        
+        return $this->render('skill/index.html.twig', [
             'page' => $page,
         ]);
     }
