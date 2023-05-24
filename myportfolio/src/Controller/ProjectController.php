54d66d6bac5a3dbb05debe9a90809a20783c2e3d
benjamingleitz <gleitz.benjamin@gmail.com>
2023-05-24 21:29:47 +0200

    création de la bdd et du css

--- myportfolio/src/Controller/ProjectController.php
+++ myportfolio/src/Controller/ProjectController.php
@@ -5,19 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
 
 #[Route('/project', name: 'project_')]
 class ProjectController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
-    {
 
+    #[Route('/', name: 'index')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
         $page = 'project';
+        $projects = $projectRepository->findAll();
 
         return $this->render('project/index.html.twig', [
-            'page'  => 'page',
-            'controller_name' => 'ProjectController',
+            'page' => $page,
+            'projects' => $projects
         ]);
     }
 }
